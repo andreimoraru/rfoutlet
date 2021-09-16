6419687513864f13a40d9988cb8209aa53c7cd87
Andrei Moraru <andrei.moraru@endava.com>
2021-09-16 14:15:33 +0300

    initial version

--- toggle.php
+++ toggle.php
@@ -5,24 +5,24 @@ header('Cache-Control: no-cache, must-revalidate');
 // Edit these codes for each outlet
 $codes = array(
     "1" => array(
-        "on" => 349491,
-        "off" => 349500
+        "on" => 5330227,
+        "off" => 5330236
     ),
     "2" => array(
-        "on" => 349635,
-        "off" => 349644
+        "on" => 5330371,
+        "off" => 5330380
     ),
     "3" => array(
-        "on" => 349955,
-        "off" => 349964
+        "on" => 5330691,
+        "off" => 5330700
     ),
     "4" => array(
-        "on" => 351491,
-        "off" => 351500
+        "on" => 5332227,
+        "off" => 5332236
     ),
     "5" => array(
-        "on" => 357635,
-        "off" => 357644
+        "on" => 5338371,
+        "off" => 5338380
     ),
 );
 
@@ -35,7 +35,7 @@ $codeSendPath = './codesend';
 $codeSendPIN = "0";
 
 // Pulse length depends on the RF outlets you are using. Use RFSniffer to see what pulse length your device uses.
-$codeSendPulseLength = "189";
+$codeSendPulseLength = "185";
 
 if (!file_exists($codeSendPath)) {
     error_log("$codeSendPath is missing, please edit the script", 0);
@@ -63,7 +63,7 @@ if ($outletLight == "6") {
 
 foreach ($codesToToggle as $codeSendCode) {
     shell_exec($codeSendPath . ' ' . $codeSendCode . ' -p ' . $codeSendPIN . ' -l ' . $codeSendPulseLength);
-    sleep(1);
+    //sleep(1);
 }
 
 die(json_encode(array('success' => true)));
